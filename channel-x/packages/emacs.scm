1e39355b16c0473214381602165a70cd216c8f17
David Wilson <david@daviwil.com>
2021-03-02 20:01:35 -0800

    channel-x: emacs: Add emacs-obs-websocket-el

---
+++ channel-x/packages/emacs.scm
@@ -0,0 +1,32 @@
+(define-module (channel-x packages emacs)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system emacs)
+  #:use-module (gnu packages emacs-xyz))
+
+;; Sacha isn't ready to publish this yet, so I'm packaging it for myself!
+(define-public emacs-obs-websocket-el
+  (let ((commit "ce6be2a417705098e26c76122eff2a0261f57d42")
+        (revision "0"))
+    (package
+      (name "emacs-obs-websocket-el")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (uri (git-reference
+               (url "https://github.com/sachac/obs-websocket-el")
+               (commit commit)))
+         (method git-fetch)
+         (sha256
+          (base32 "0fdjpxc75jk9cxgg66p24z1n5w9zzqbrci5mlqam31a2caip9bmx"))
+         (file-name (git-file-name name version))))
+      (build-system emacs-build-system)
+      (propagated-inputs `(("emacs-websocket" ,emacs-websocket)))
+      (home-page "https://github.com/sachac/obs-websocket-el")
+      (synopsis "Emacs package for remote control of OBS")
+      (description
+       "obs-websocket-el is a client for the obs-websocket plugin for OBS.
+It enables you to remotely control your streaming or recording session from
+Emacs!")
+      (license license:gpl3+))))
